9742c8ae742ecd6cc99ed29126a54f07c5d9fa10
Fahad AlJassar <iialjassar@gmail.com>
2024-08-04 14:17:30 +0300

    create main app

---
+++ smart/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+
+# Create your views here.
+
+
+
+
+def home_view(request:HttpRequest):
+
+
+    return render(request,"main/home.html") 
+    
+    
+
